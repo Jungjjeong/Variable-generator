448765cfc83c65075c52d78baed6fec428d3ea7f
Jungjjeong <sky990106@dgu.ac.kr>
2022-02-22 11:36:43 +0900

    fix : Eslint, Prettier 충돌 문제 해결 및 코드 초기화

---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" style="width:20%">
+    <HelloWorld msg="변수명 검색기"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
